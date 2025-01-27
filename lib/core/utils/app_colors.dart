e69b65e8f0ba21a650d8a7677ae5100da284b14d
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-27 09:46:06 +0200

    Add utility classes for app colors and styles
    Created `AppColors` class to define application color themes and `TextStyles` class to provide responsive text styles using the SpaceGrotesk font. Also, updated `pubspec.yaml` to include font assets and asset generation configuration.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorTheme = Color(0xffA3D80D);
+  static const Color secondaryColorTheme = Color(0xffF1F9DB);
+  static const Color blackColorTheme = Color(0xff171816);
+  static const Color secondaryColorFirst = Color(0xffF1F9DB);
+  static const Color secondaryColorSecond = Color(0xffDADADA);
+}
